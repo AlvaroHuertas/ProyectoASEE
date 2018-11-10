7a37714b2f46603e20b05ec85f57aaa068ede997
AlvaroHuertas <alvarohuertas8@gmail.com>
2018-11-10 20:31:50 +0100

    Añadida lista al fragmentOne y arreglados algunos errores del layout principal

--- app/src/main/java/com/unex/proyectoasee_nogymmembership/FragmentOne.java
+++ app/src/main/java/com/unex/proyectoasee_nogymmembership/FragmentOne.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,6 +17,7 @@ import android.view.ViewGroup;
  */
 public class FragmentOne extends Fragment {
 
+    ListView listCategories;
 
     public FragmentOne() {
         // Required empty public constructor
@@ -22,8 +27,25 @@ public class FragmentOne extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_one, container, false);
+
+        //Inflate del layout para el fragment
+        View view = inflater.inflate(R.layout.fragment_fragment_one, container, false);
+
+        //Definimos la lista a partir del view inflated
+        listCategories = (ListView) view.findViewById(R.id.listCategories);
+
+        //Utilizamos un array de prueba
+        //TODO - Establecer este array a partir de un .xml que contenga las categorias
+        ArrayList<String> test = new ArrayList<>();
+        test.add("Hola");
+        test.add("mundo");
+
+        //Adapter para la lista
+        ArrayAdapter adapt;
+        adapt = new ArrayAdapter(getContext(), android.R.layout.simple_list_item_1, test);
+        listCategories.setAdapter(adapt);
+
+        return view;
     }
 
 }
