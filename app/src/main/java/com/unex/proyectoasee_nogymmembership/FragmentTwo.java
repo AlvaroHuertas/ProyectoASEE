ee1f1f8066cc65b8f86998297259af43dcac4a51
prodriqu <prodrigulu@alumnos.unex.es>
2019-01-06 13:45:50 +0100

    -FragmentTwo modificado: Funcionalidad Networking separada

--- app/src/main/java/com/unex/proyectoasee_nogymmembership/FragmentTwo.java
+++ app/src/main/java/com/unex/proyectoasee_nogymmembership/FragmentTwo.java
@@ -18,6 +18,7 @@ import com.unex.proyectoasee_nogymmembership.DBUtils.ExerciseCRUD;
 import com.unex.proyectoasee_nogymmembership.Models.Exercise;
 import com.unex.proyectoasee_nogymmembership.Models.ExerciseList;
 import com.unex.proyectoasee_nogymmembership.Networking.NetworkUtils;
+import com.unex.proyectoasee_nogymmembership.Networking.NetworkingAndroidHttpClientJSON;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,9 +46,12 @@ public class FragmentTwo extends Fragment {
     private static final String MUSCLES_TAG = "muscles";
     private static final String IMAGE_TAG = "image";
 
+
+
     private static RecyclerView mExercisesRecycler=null;
 
-
+    ExerciseAdapter mAdapter;
+    ExerciseList exerciseList;
 
 
     public FragmentTwo() {
@@ -63,116 +67,17 @@ public class FragmentTwo extends Fragment {
        this.mExercisesRecycler=(RecyclerView) rootView.findViewById(R.id.exercises_recycler);
 
         //Ejecutar petición GET
-        new HttpGetTask(getActivity()).execute();
+        NetworkingAndroidHttpClientJSON petition=new NetworkingAndroidHttpClientJSON();
 
+        this.exerciseList=petition.getExerciseList();
+
+        LinearLayoutManager lay_Manager = new LinearLayoutManager(getActivity());
+        mExercisesRecycler.setLayoutManager(lay_Manager);
+
+        mAdapter = new ExerciseAdapter(getActivity(), exerciseList);
+        mExercisesRecycler.setAdapter(mAdapter);
 
         return rootView;
     }
-
-    private class HttpGetTask extends AsyncTask<Void, Void, ExerciseList> {
-        private static final String BASE_URL ="wger.de";
-        private static final String JSON_SEG = "api";
-        private static final String JSON_SEG2 ="v2";
-        private static final String JSON_SEG_EX ="exercise";
-        private static final String JSON_SEG_IMG="exerciseimage";
-        private static final String LANGUAGE_P = "language";
-        private static final String EQUIPMENT_P = "equipment";
-        private static final String IMAGE = "image";
-
-        private Context mContext;
-
-        private ExerciseAdapter mAdapter;
-        private View rootView;
-
-        private ExerciseList exerciseList;
-
-        public HttpGetTask(Context context){
-            mContext=context;
-        }
-
-        @Override
-        protected ExerciseList doInBackground(Void... params) {
-            URL queryURL;
-            JSONObject[] result=new JSONObject[2];
-
-            //Construir URI de EJERCICIOS con los campos definidos arriba
-            queryURL = NetworkUtils.buildURL(BASE_URL,
-                    new String[]{JSON_SEG,JSON_SEG2,JSON_SEG_EX},
-                    new Pair(LANGUAGE_P, "2"),
-                    new Pair(EQUIPMENT_P, "7"));
-            result[0] = NetworkUtils.getJSONResponse(queryURL);
-
-
-            //Construir URI de IMAGENES
-            queryURL = NetworkUtils.buildURL(BASE_URL,
-                    new String[]{JSON_SEG,JSON_SEG2,JSON_SEG_IMG});
-            result[1] = NetworkUtils.getJSONResponse(queryURL);
-
-            if(result != null)
-                return jsonToList(result);
-
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(ExerciseList result) {
-            ExerciseList exerciseList=result;
-            List<Exercise> aux=exerciseList.getElements();
-            //ExerciseCRUD crud = ExerciseCRUD.getInstance(getContext());
-            /*List<Exercise> prueba=crud.getAll();*/
-            //crud.deleteAll();
-
-            if(aux != null) {
-
-                for (int i = 0; i < aux.size(); i++) {
-                    Exercise item = aux.get(i);
-                    //long id = crud.insert(item);
-                    //item.setExerciseId(id);
-                }
-            }
-            LinearLayoutManager lay_Manager=new LinearLayoutManager(mContext);
-            mExercisesRecycler.setLayoutManager(lay_Manager);
-
-            mAdapter= new ExerciseAdapter(mContext, exerciseList);
-            mExercisesRecycler.setAdapter(mAdapter);
-
-        }
-
-    }
-
-    public ExerciseList jsonToList(JSONObject[] responseObject) {
-        List<Exercise> exercisesList = new ArrayList<>();
-        ExerciseList result=new ExerciseList();
-
-        try {
-
-        if(responseObject[0] != null) {
-            JSONArray exercises = responseObject[0]
-                    .getJSONArray(EXERCISE_TAG);
-            for (int idx = 0; idx < exercises.length(); idx++) {
-
-                // Get single exercise data - a Map
-                JSONObject exercise = (JSONObject) exercises.get(idx);
-
-                Exercise exerciseObj = new Exercise(idx, exercise.get(NAME_TAG).toString(), exercise.get(DESCRIPTION_TAG).toString(), "");
-
-                exercisesList.add(exerciseObj);
-                result.setElements(exercisesList);
-                // Summarize exercise data as a string and add it to
-                // result
-                /*result.add(NAME_TAG + ":"
-                        + exercise.get(NAME_TAG) + ","
-                        + DESCRIPTION_TAG + ":"
-                        + exercise.getString(DESCRIPTION_TAG));*/
-            }
-        }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-
 
 }
---
+++ app/src/main/java/com/unex/proyectoasee_nogymmembership/Networking/NetworkingAndroidHttpClientJSON.java
@@ -0,0 +1,114 @@
+package com.unex.proyectoasee_nogymmembership.Networking;
+
+import android.os.AsyncTask;
+import android.util.Pair;
+import android.view.View;
+
+import com.unex.proyectoasee_nogymmembership.Models.Exercise;
+import com.unex.proyectoasee_nogymmembership.Models.ExerciseList;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NetworkingAndroidHttpClientJSON {
+
+    //Definición de clases JSON
+    private static final String EXERCISE_TAG = "results";
+    private static final String DESCRIPTION_TAG = "description";
+    private static final String NAME_TAG = "name";
+    private static final String CATEGORY_TAG = "category";
+    private static final String MUSCLES_TAG = "muscles";
+    private static final String IMAGE_TAG = "image";
+    private ExerciseList exerciseList;
+
+    public NetworkingAndroidHttpClientJSON() {
+        HttpGetTask httpGetTask=new HttpGetTask();
+        httpGetTask.execute();
+        this.exerciseList=httpGetTask.getExerciseList();
+    }
+
+    public ExerciseList getExerciseList() {
+        return exerciseList;
+    }
+
+    private class HttpGetTask extends AsyncTask<Void, Void, ExerciseList> {
+        private static final String BASE_URL = "wger.de";
+        private static final String JSON_SEG = "api";
+        private static final String JSON_SEG2 = "v2";
+        private static final String JSON_SEG_EX = "exercise";
+        private static final String JSON_SEG_IMG = "exerciseimage";
+        private static final String LANGUAGE_P = "language";
+        private static final String EQUIPMENT_P = "equipment";
+        private static final String IMAGE = "image";
+        private ExerciseList exerciseList;
+
+        private View rootView;
+
+        public HttpGetTask(){
+            this.exerciseList=new ExerciseList();
+        }
+
+        public ExerciseList getExerciseList() {
+            //Wait until exercises are load
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return exerciseList;
+        }
+
+        @Override
+        protected ExerciseList doInBackground(Void... params) {
+            URL queryURL;
+            JSONObject[] result = new JSONObject[2];
+
+            //Construir URI de EJERCICIOS con los campos definidos arriba
+            queryURL = NetworkUtils.buildURL(BASE_URL,
+                    new String[]{JSON_SEG, JSON_SEG2, JSON_SEG_EX},
+                    new Pair(LANGUAGE_P, "2"),
+                    new Pair(EQUIPMENT_P, "7"));
+            result[0] = NetworkUtils.getJSONResponse(queryURL);
+
+
+            //Construir URI de IMAGENES
+            queryURL = NetworkUtils.buildURL(BASE_URL,
+                    new String[]{JSON_SEG, JSON_SEG2, JSON_SEG_IMG});
+            result[1] = NetworkUtils.getJSONResponse(queryURL);
+
+            jsonToList(result);
+
+            return null;
+        }
+
+        public void jsonToList(JSONObject[] responseObject) {
+            List<Exercise> exercisesList = new ArrayList<>();
+            try {
+
+                if (responseObject[0] != null) {
+                    JSONArray exercises = responseObject[0]
+                            .getJSONArray(EXERCISE_TAG);
+                    for (int idx = 0; idx < exercises.length(); idx++) {
+                        // Get single exercise data - a Map
+                        JSONObject exercise = (JSONObject) exercises.get(idx);
+
+                        Exercise exerciseObj = new Exercise(idx, exercise.get(NAME_TAG).toString(), exercise.get(DESCRIPTION_TAG).toString(), "");
+
+                        exercisesList.add(exerciseObj);
+                    }
+                 this.exerciseList.setElements(exercisesList);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
